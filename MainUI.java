e0bc53d4fb6abdb111d58de6c9ee966fe4d0ac07
Sebastian-ceu <sebastian.romanimaurolagoitia@usp.ceu.es>
2026-09-24 15:29:36 +0200

    Added the MainUI
    Added the mainUI added with Singleton method implemented. Needs iterating on in the future.

---
+++ MainUI.java
@@ -0,0 +1,36 @@
+class MainUIManager{
+       public static MainUIManager instance;
+       public String option = "MENU";
+
+     
+       public static MainUIManager getInstance(){
+	       if (instance == null){
+		       instance == new MainUIManager(); 
+	       }
+	       return instance;
+       }
+
+       public void setOption(String option){
+	       this.option = option;
+       }
+
+       public String getOption(){
+	       return option;
+       }       
+}
+
+class SelectionChoice {
+	public void display() {
+		MainUIManager manager = MainUIManager.getInstance();
+		System.out.println("Current Option Selected: " + manager.getOption();
+	}
+}
+
+public class MainUI{
+	public static void main(String[] args)
+	{
+		MainUIManager manager = MainUI.getINstance();
+		
+		new SelectionChoice().display();
+	}
+} 
